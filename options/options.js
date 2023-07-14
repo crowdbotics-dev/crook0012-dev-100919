f2dab9c29ed4f60195d64f2218b3757d20f3e5be
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-07-14 16:11:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "crook0012-dev-100919",
   url: "https://crook0012-dev-100919.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/crookdevAPI/api.js
+++ store/crookdevAPI/api.js
@@ -1,52 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const crookdevAPI = axios.create({
   baseURL: "https://crook0012-dev-100919.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return crookdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return crookdevAPI.post(`/api/v1/login/`, payload)
+  return crookdevAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return crookdevAPI.post(`/api/v1/signup/`, payload)
+  return crookdevAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return crookdevAPI.post(`/rest-auth/login/`, payload)
+  return crookdevAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return crookdevAPI.get(`/rest-auth/logout/`)
+  return crookdevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return crookdevAPI.post(`/rest-auth/logout/`)
+  return crookdevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return crookdevAPI.post(`/rest-auth/password/change/`, payload)
+  return crookdevAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return crookdevAPI.post(`/rest-auth/password/reset/`, payload)
+  return crookdevAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return crookdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return crookdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return crookdevAPI.post(`/rest-auth/registration/`, payload)
+  return crookdevAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return crookdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return crookdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return crookdevAPI.get(`/rest-auth/user/`)
+  return crookdevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return crookdevAPI.put(`/rest-auth/user/`, payload)
+  return crookdevAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return crookdevAPI.patch(`/rest-auth/user/`, payload)
+  return crookdevAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -62,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
